5eb5e7a6cb154d81be5a797d0de0be2ac3e08a4a
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-03-21 03:17:06 +0300

    Update Contents.swift

--- MultiProtocol.playground/Contents.swift
+++ MultiProtocol.playground/Contents.swift
@@ -3,7 +3,63 @@
 /*
  Şimdi Drone eklendiği zaman elimizde Airplane,Bird,Drone classları olacak bunlardan bazıları canlı bazıları ise cansızdır. Bunun için ileride ayrı bir canlı varlık eklendiği zaman kullanmak için iki Adet protocol üreterek bunları ayrılmasını sağlayabilirim
  */
-protocol Flyable {
+
+//MARK: Before
+
+protocol Flyable : AnyObject {
+    func fly() -> String
+}
+
+class Bird: Flyable {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func fly() -> String {
+        return "\(name) is flying"
+    }
+}
+
+class Airplane: Flyable {
+    var model: String
+
+    init(model: String) {
+        self.model = model
+    }
+
+    func fly() -> String {
+        return "Airplane \(model) is flying"
+    }
+}
+
+class Airport {
+    var flyables: [Flyable] = []
+
+    func addFlyable(_ flyable: Flyable) {
+        flyables.append(flyable)
+    }
+
+    func listFlyingObjects() {
+        for item in flyables {
+            print(item.fly())
+        }
+    }
+}
+
+
+let bird = Bird(name: "Eagle")
+let airplane = Airplane(model: "Boeing 747")
+
+let airport = Airport()
+airport.addFlyable(bird)
+airport.addFlyable(airplane)
+
+airport.listFlyingObjects()
+
+//MARK: After
+protocol Flyable : AnyObject{
     func fly() -> String
 }
 
